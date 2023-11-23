39d3bdec2c5788676505de3ec4796e19ed2de3f1
GuNShOtzZ <kevinloeding@outlook.de>
2023-11-23 11:23:06 +0100

    Add function to bookmark questions

--- js/question.js
+++ js/question.js
@@ -3,6 +3,8 @@ console.clear();
 const answer = document.querySelector('[data-js="answer"]');
 const answerButton = document.querySelector('[data-js="answer-button"]');
 
+const bookmark = document.querySelector('[data-js="bookmark"]');
+
 answerButton.addEventListener("click", () => {
     answer.classList.toggle("hide-answer");
 
@@ -11,4 +13,15 @@ answerButton.addEventListener("click", () => {
     } else {
         answerButton.textContent = "Antwort zeigen"
     }
+});
+
+bookmark.addEventListener("click", () => {
+
+    if (!bookmark.classList.contains("bookmarked")) {
+        bookmark.innerHTML = '<img src="img/icons/bookmarked.png" alt="Bookmark Icon" width="16" height="16"/>';
+    } else {
+        bookmark.innerHTML = '<img src="img/icons/not-bookmarked.png" alt="Bookmark Icon" width="16" height="16"/>';
+    }
+
+    bookmark.classList.toggle("bookmarked");
 });
